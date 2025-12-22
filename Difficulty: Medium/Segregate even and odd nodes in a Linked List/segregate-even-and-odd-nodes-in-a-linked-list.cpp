ae0dd0e0c5814c9652ee439109c2606c8f75844b
KshitijS2683 <117576793+KshitijS2683@users.noreply.github.com>
2025-12-22 21:43:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Segregate even and odd nodes in a Linked List/segregate-even-and-odd-nodes-in-a-linked-list.cpp
+++ Difficulty: Medium/Segregate even and odd nodes in a Linked List/segregate-even-and-odd-nodes-in-a-linked-list.cpp
@@ -15,50 +15,70 @@ struct Node
 class Solution {
   public:
     Node* divide(Node* head) {
-        // code here
-        Node* odd = NULL;
-        Node* even = NULL;
-        Node* oddhead = NULL;
-        Node* evenhead = NULL;
-        Node* temp = head;
-        while(temp != NULL)
-        {
-            if(temp->data%2 == 0)
-            {
-                if(evenhead == NULL)
-                {
-                    evenhead = new Node(temp->data);
-                    even = evenhead;
-                }
-                else
-                {
-                    Node* next = new Node(temp->data);
-                    even->next = next;
-                    even = even->next;
-                }
-            }
-            else
-            {
-                if(oddhead == NULL)
-                {
-                    oddhead = new Node(temp->data);
-                    odd = oddhead;
-                }
-                else
-                {
-                    Node* next = new Node(temp->data);
-                    odd->next = next;
-                    odd = odd->next;
-                }
-            }
-            temp = temp->next;
+    Node* dummy = new Node(0);
+    dummy->next = head;
+    Node* temp1 = dummy;
+    Node* temp2 = dummy;
+    while (temp2->next && temp2->next->data % 2 == 0) {
+        temp2 = temp2->next;
+    }
+    temp1 = temp2;
+    while (temp2->next) {
+        if (temp2->next->data % 2 == 0) {
+            Node* h = temp2->next;
+            temp2->next = h->next;
+            h->next = temp1->next;
+            temp1->next = h;
+            temp1 = h;
+        } else {
+            temp2 = temp2->next;
         }
-        if(even == NULL)
-        {
-            return oddhead;
-        }
-        even->next = oddhead;
-        return evenhead;
+    }
+    return dummy->next;
+        
+        // Node* odd = NULL;
+        // Node* even = NULL;
+        // Node* oddhead = NULL;
+        // Node* evenhead = NULL;
+        // Node* temp = head;
+        // while(temp != NULL)
+        // {
+        //     if(temp->data%2 == 0)
+        //     {
+        //         if(evenhead == NULL)
+        //         {
+        //             evenhead = new Node(temp->data);
+        //             even = evenhead;
+        //         }
+        //         else
+        //         {
+        //             Node* next = new Node(temp->data);
+        //             even->next = next;
+        //             even = even->next;
+        //         }
+        //     }
+        //     else
+        //     {
+        //         if(oddhead == NULL)
+        //         {
+        //             oddhead = new Node(temp->data);
+        //             odd = oddhead;
+        //         }
+        //         else
+        //         {
+        //             Node* next = new Node(temp->data);
+        //             odd->next = next;
+        //             odd = odd->next;
+        //         }
+        //     }
+        //     temp = temp->next;
+        // }
+        // if(even == NULL)
+        // {
+        //     return oddhead;
+        // }
+        // even->next = oddhead;
+        // return evenhead;
         
     }
 };
