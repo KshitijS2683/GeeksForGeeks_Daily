c7577c9de46dbbf1fed5ada3d4c33ab34126a22f
KshitijS2683 <117576793+KshitijS2683@users.noreply.github.com>
2026-01-26 22:28:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Power of 2/power-of-2.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  public:
+    bool isPowerofTwo(int n) {
+        // code here
+        if(n == 1)
+        {
+            return true;
+        }
+        if(n == 0)
+        {
+            return false;
+        }
+        for(int i = 2;i<n;i=i*2)
+        {
+            if(n%i != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
