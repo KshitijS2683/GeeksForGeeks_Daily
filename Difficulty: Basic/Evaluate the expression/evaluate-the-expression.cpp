37674babf3591ad364cb15d5b8fbe7e1fbe69386
KshitijS2683 <117576793+KshitijS2683@users.noreply.github.com>
2026-02-08 22:50:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Evaluate the expression/evaluate-the-expression.cpp
@@ -0,0 +1,16 @@
+
+class Solution {
+  public:
+    int evaluate_exp(int n) {
+        // Code here
+        if(n%2 == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return -1;
+        }
+        
+    }
+};
