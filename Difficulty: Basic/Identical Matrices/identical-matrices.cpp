b890b360064ac8b17dd4ffcedaa36efa75e814d6
KshitijS2683 <117576793+KshitijS2683@users.noreply.github.com>
2026-05-04 18:16:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Identical Matrices/identical-matrices.cpp
@@ -0,0 +1,10 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int areMatricesIdentical(int N, vector<vector<int>> Grid1,
+                             vector<vector<int>> Grid2) {
+        // code here
+        return Grid1 == Grid2;
+    }
+};
