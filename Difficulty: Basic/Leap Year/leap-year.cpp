a1c7d8600fe891f01207b1858b57fa2e95a17182
KshitijS2683 <117576793+KshitijS2683@users.noreply.github.com>
2026-05-30 23:20:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Leap Year/leap-year.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    bool checkYear(int n) {
+        // code here
+        if(n%4 == 0 && n%100 != 0)
+        {
+            return true;
+        }
+        if(n%400 == 0)
+        {
+            return true;
+        }
+        return false;
+    }
+};
