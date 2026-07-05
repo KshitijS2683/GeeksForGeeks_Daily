a61cb515b5fb07a8d125f710d1f1ccdf7c0f73dc
KshitijS2683 <117576793+KshitijS2683@users.noreply.github.com>
2026-07-05 22:42:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/While Loop/while-loop.cpp
@@ -0,0 +1,11 @@
+class Solution {
+  public:
+    void utility(int x) {
+        // code here
+        for(int i = x;i>=0;i--)
+        {
+            cout << i;
+            cout << ' ';
+        }
+    }
+};
