5fae731443dc924fce4707726d8371e9b79142b8
KshitijS2683 <117576793+KshitijS2683@users.noreply.github.com>
2026-07-27 22:42:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/First Occurrence in Sorted/first-occurrence-in-sorted.cpp
@@ -0,0 +1,27 @@
+class Solution {
+  public:
+    int firstSearch(vector<int> &arr, int k) {
+        // code here
+        int start = 0;
+        int end = arr.size()-1;
+        int ans = -1;
+        while(start<=end)
+        {
+            int mid = (start+end)/2;
+            if(arr[mid] == k)
+            {
+                ans = mid;
+                end = mid - 1;
+            }
+            else if(arr[mid] < k)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+};
